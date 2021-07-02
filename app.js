fec45bd8b089550b819a0f98ee5f9c7c8dea62a6
AlanRacciatti <alanracciatti1220@gmail.com>
2021-07-02 18:39:26 -0300

    Creados login y register. Cambios estéticos al home

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const { resolveSoa } = require('dns');
 const express = require('express');
 const app = express();
 const path = require('path');
@@ -10,4 +11,12 @@ app.listen(process.env.PORT || 3000, () => {
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, './views/home.html'));
+});
+
+app.get('/register.html', (req, res) => {
+  res.sendFile(path.join(__dirname, './views/register.html'));
+});
+
+app.get('/login.html', (req, res) => {
+  res.sendFile(path.join(__dirname, './views/login.html'));
 });
